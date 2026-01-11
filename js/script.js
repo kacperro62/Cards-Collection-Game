18759a295189b37b699f4df3ec95bab6535dbc31
Kacperro <kacperbutora@gmail.com>
2026-01-11 12:49:01 +0100

    feat: added temporary chest rarity and opening system

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
+
+// Sidebar menu
+
 const menuTriggerEl = document.querySelector(".menu-trigger");
 const sideBarEl = document.querySelector("nav");
 const closeEl = document.querySelector("nav span");
 
 menuTriggerEl.addEventListener("click", () => {
-    sideBarEl.classList.add("open");
+    sideBarEl.classList.remove("open");
 });
 
 closeEl.addEventListener("click", () => {
-    sideBarEl.classList.remove("open");
+    sideBarEl.classList.add("open");
+});
+
+// Chests rarity and opening system
+
+const chest = document.querySelector(".chest");
+const result = document.querySelector(".result")
+
+chest.addEventListener("click", () => {
+    let randomChance = Math.floor((Math.random() * 100) + 1);
+    result.textContent = randomChance;
+
+    if (randomChance > 0 && randomChance <= 50)
+    {
+        document.writeln("1 in 2");
+    }
+    else if (randomChance > 50 && randomChance <= 75)
+    {
+        document.writeln("1 in 4");
+    } 
+    else if (randomChance > 75 && randomChance <= 90)
+    {
+        document.writeln("1 in 7");
+    }
+    else if (randomChance > 90 && randomChance <= 99)
+    {
+        document.writeln("1 in 11");
+    }
+    else 
+    {
+        document.writeln("1 in 100");
+    };
 });
