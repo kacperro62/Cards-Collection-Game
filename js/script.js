88855c83a48dfb1df86bd0c9121c89cb1eaa5c6c
Kacperro <kacperbutora@gmail.com>
2026-01-11 13:01:20 +0100

    feat: fixed chest rarity and opening system

--- js/script.js
+++ js/script.js
@@ -20,26 +20,25 @@ const result = document.querySelector(".result")
 
 chest.addEventListener("click", () => {
     let randomChance = Math.floor((Math.random() * 100) + 1);
-    result.textContent = randomChance;
 
     if (randomChance > 0 && randomChance <= 50)
     {
-        document.writeln("1 in 2");
+        result.textContent = "1 in 2";
     }
     else if (randomChance > 50 && randomChance <= 75)
     {
-        document.writeln("1 in 4");
+        result.textContent = "1 in 4";
     } 
     else if (randomChance > 75 && randomChance <= 90)
     {
-        document.writeln("1 in 7");
+        result.textContent = "1 in 7";
     }
     else if (randomChance > 90 && randomChance <= 99)
     {
-        document.writeln("1 in 11");
+        result.textContent = "1 in 11";
     }
     else 
     {
-        document.writeln("1 in 100");
+        result.textContent = "1 in 100";
     };
 });
